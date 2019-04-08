d274f079d6ce252d96315c10d391307b84af08c2
Johannes Jordan <johannes.jordan@physiologie.uni-freiburg.de>
2019-04-08 15:28:21 +0200

    move colormap application to colormap namespace
    for code re-use elsewhere

--- colormap.h
+++ colormap.h
@@ -1,10 +1,18 @@
 #ifndef COLORMAP_H
 #define COLORMAP_H
 
+#include <QPixmap>
 #include <opencv2/core/core.hpp>
 
 namespace colormap {
-    extern std::array<cv::Vec3b, 256> magma;
+
+	// convert Mat1b
+	QPixmap apply(cv::Mat1b &source);
+	// convert any matrix (includes conversion to Mat1b)
+	QPixmap apply(cv::Mat &source, double scale);
+
+	// Magma color map
+	extern std::array<cv::Vec3b, 256> magma;
 };
 
 #endif
